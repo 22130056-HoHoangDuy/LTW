f476ef0b8c56af6a52d0b6f48f0981280d117117
lam doanh <you@example.com>
2025-11-06 17:37:02 +0700

    add footer and header to product detail and cart page

--- FE/js/productList.js
+++ FE/js/productList.js
@@ -1,11 +1,22 @@
 const backBtn = document.getElementById("backToTop");
+const footer = document.getElementById("footer-frame");
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 400) {
-    backBtn.classList.add("show");
-  } else {
-    backBtn.classList.remove("show");
-  }
+    const footerTop = footer.getBoundingClientRect().top;//get footer's position in page
+    const windowHeight = window.innerHeight;
+    if (window.scrollY > 400) {
+        backBtn.classList.add("show");
+    } else {
+        backBtn.classList.remove("show");
+    }
+    if (footerTop < windowHeight) {
+        backBtn.style.background = "white";
+        backBtn.style.color = "blue";
+    } else {
+        backBtn.style.background = "#003dd4";
+        backBtn.style.color = "white";
+    }
+
 });
 backBtn.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({top: 0, behavior: "smooth"});
 });
